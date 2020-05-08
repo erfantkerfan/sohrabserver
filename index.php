2af1f16431fd0744f63ffba141b18ad2db3409c5
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-05-09 02:58:30 +0430

    fix ui

--- index.php
+++ index.php
@@ -18,12 +18,18 @@ https://github.com/erfantkerfan
     <title>ققنوس</title>
     <link rel='icon' href='img/icon.webp' type='image/webp'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta name='description' content="">
-    <meta name='authors' content="">
+    <meta name='description' content="> <meta name='authors content=">
 
-    <script src='/js/jquery-1.11.0-jquery.min.js' type="javascript"></script>
-    <script src='/js/jqueryui-1.10.3-jquery-ui.min.js' type="javascript"></script>
-    <script src='/js/jqueryui-touch-punch-0.2.2-jquery.ui.touch-punch.min.js' type="javascript"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="js/jqueryui-touch-punch-0.2.2-jquery.ui.touch-punch.min.js"></script>
+
+    <script>
+        $(function () {
+            $("#sortable").sortable();
+            $( "#sortable" ).disableSelection();
+        });
+    </script>
     <style>
         #sortable {
             list-style-type: none;
@@ -31,45 +37,43 @@ https://github.com/erfantkerfan
             padding: 0;
             width: 100%;
         }
+        #sortable li { height: 100%; margin: 0px; border: 0px; padding: 0px}
+        #sortable li img{width: 100%; margin: 0px; border: 0px; padding: 0px;}
     </style>
-    <script type="javascript">
-        $(function () {
-            $('#sortable').sortable({forceHelperSize: true});
-            // $('#sortable').disableSelection();
-        });
-
-    </script>
 </head>
 <body>
-<div>
-    <ol id="sortable">
-        <?php
-        $file = 10;
-        $latest = 5;
+<div class='container'>
 
-        $folder = isset($_GET["week"]) ? str_pad($_GET["week"], 2, "0", STR_PAD_LEFT) : str_pad($latest, 2, "0", STR_PAD_LEFT);
-        if ($folder > $latest) {
-            header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-            die();
-        }
+    <div>
+        <ul id="sortable">
+            <?php
+            $file = 10;
+            $latest = 5;
 
-        $numbers = range(1, $file);
-        $numbers = array_map(function ($item) {
-            return str_pad($item, 2, "0", STR_PAD_LEFT);
-        }, $numbers);
-        shuffle($numbers);
+            $folder = isset($_GET["week"]) ? str_pad($_GET["week"], 2, "0", STR_PAD_LEFT) : $latest;
+            if ($folder > $latest) {
+                header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+                die();
+            }
 
-        foreach ($numbers as $number) {
-            $x =
-                "
-                    <li class='ui-state-default' style='height: 100%; margin: 0px; border: 0px; padding: 0px'>
-                        <img style='width: 100%; margin: 0px; border: 0px; padding: 0px;' src='/img/" . $folder . "/" . $number . ".webp'>
+            $numbers = range(1, $file);
+            $numbers = array_map(function ($item) {
+                return str_pad($item, 2, "0", STR_PAD_LEFT);
+            }, $numbers);
+            shuffle($numbers);
+
+            foreach ($numbers as $number) {
+                $x =
+                    "
+                    <li class='ui-state-default'>
+                        <img src='/img/" . $folder . "/" . $number . ".webp'>
                     </li>
                     ";
-            echo $x;
-        }
-        ?>
-    </ol>
+                echo $x;
+            }
+            ?>
+        </ul>
+    </div>
 </div>
 </body>
 </html>
