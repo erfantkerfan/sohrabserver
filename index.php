58f17e56884e00bdaa7d16dd1d550c901072cf5b
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-05-15 15:51:09 +0430

    added week 6

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ https://github.com/erfantkerfan
 -->
 <?php
 $file = 10;
-$latest = 5;
+$latest = 6;
 
 $folder = isset($_GET["week"]) ? str_pad($_GET["week"], 2, "0", STR_PAD_LEFT) : str_pad($latest, 2, "0", STR_PAD_LEFT);
 if ($folder > $latest) {
